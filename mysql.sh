0bb0e6e4c71c953437c4ead92facb44d60c0e781
Arisa Onwuka <onwuka.arisa@gmail.com>
2023-06-06 21:46:57 +0100

    Finally got the mysql.sh script working. The aim here is to run mysql commands in the bash script. The "-e" flag of mysql was used to achieve this, passing all the mysql statements as command line parameters.

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Install MySQL server
 sudo apt-get install mysql-server 
@@ -22,17 +22,17 @@ mysql_secure_installation
 
 
 echo "Creating a database called 'example_database'"
-mysql -u root -p -e "CREATE DATABASE "example_database"";
+mysql -u root -p -e 'CREATE DATABASE 'example_database'';
 echo "Database successfully created....."
 
 
 echo
 echo
-echo "Creating a new user called 'example_user' with default passowrd 'Password2$'
+echo "Creating a new user called 'example_user' with default passowrd 'Password2$'"
 mysql -u root -p -e "CREATE USER 'example_user'@'%' IDENTIFIED WITH mysql_native_password BY 'Password2$';"
 echo "User Successfully created...."
 
-echo
+echo 
 echo
 echo "Granting example_user full access to example_database"
 mysql -u root -p -e "GRANT ALL ON example_database.* TO 'example_user'@'%';"
@@ -44,16 +44,14 @@ mysql -u example_user -p
 
 
 echo "Creating todo_list in example database..."
-mysql -u root -p -e "CREATE TABLE example_database.todo_list(item_id INT AUTO_INCREMENT,content VARCHAR(255),PRIMARY KEY(item_id));" 
+mysql -u root -p -e 'CREATE TABLE example_database.todo_list(item_id INT AUTO_INCREMENT,content VARCHAR(255),PRIMARY KEY(item_id));'
 
 
 echo "Inserting items into the table..."
-mysql -u root -p -e "INSERT INTO example_database.todo_list (content) VALUES ('My first important item');"
-mysql -u root -p -e "INSERT INTO example_database.todo_list (content) VALUES ('My second important item');"
-mysql -u root -p -e "INSERT INTO example_database.todo_list (content) VALUES ('My third important item');"
-mysql -u root -p -e "INSERT INTO example_database.todo_list (content) VALUES ('and this one more thing');"
+mysql -u root -p -e 'INSERT INTO example_database.todo_list (content) VALUES ("My first important item");'
+mysql -u root -p -e 'INSERT INTO example_database.todo_list (content) VALUES ("My second important item");'
+mysql -u root -p -e 'INSERT INTO example_database.todo_list (content) VALUES ("My third important item");'
+mysql -u root -p -e 'INSERT INTO example_database.todo_list (content) VALUES ("and this one more thing");'
   
 
 cp todo_list.php /var/www/projectLEMP/todo_list.php
-
-
